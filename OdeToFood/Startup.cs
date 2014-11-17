f389cff5479bc4903c1f2dbe4a6ee9f69189bd23
rohithgopi <rohi.torque@gmail.com>
2014-11-17 12:37:16 +0530

    OdeToCodeStart

---
+++ OdeToFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OdeToFood.Startup))]
+namespace OdeToFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
